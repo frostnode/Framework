6fa5b40e243c230869ad721a488171d0ba0fdf47
Kjell-Magnus Vike <magnus@vike.se>
2018-03-20 22:05:15 +0100

    Added some field documentation

--- app/Pages/Page.php
+++ app/Pages/Page.php
@@ -1,5 +1,30 @@
 <?php
 
+/*
+ * Fields options
+ *
+ * Required
+ * 'name' => 'your field name'
+ * 'type' => 'text|textarea etc'
+ *
+ * $options = [
+ *   'wrapper' => ['class' => 'form-group'],
+ *   'attr' => ['class' => 'form-control'],
+ *   'help_block' => [
+ *       'text' => null,
+ *       'tag' => 'p',
+ *       'attr' => ['class' => 'help-block']
+ *   ],
+ *   'default_value' => null, // Fallback value if none provided by value property or model
+ *   'label' => $this->name,  // Field name used
+ *   'label_show' => true,
+ *   'label_attr' => ['class' => 'control-label', 'for' => $this->name],
+ *   'errors' => ['class' => 'text-danger'],
+ *   'rules' => [],           // Validation rules
+ *   'error_messages' => []   // Validation error messages
+ * ]
+ */
+
 namespace App\Pages;
 
 use Modules\Page\Entities\PageType;
