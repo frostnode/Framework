29689bee0888cb98fe5ebaff7d311359b7e78f74
Kjell-Magnus Vike <magnus@vike.se>
2018-04-03 22:42:58 +0200

    Added new logo
    Work in progress, but it'll do..

--- modules/Core/resources/views/partials/navbar.blade.php
+++ modules/Core/resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
         <a class="navbar-item" href="{{ route('admin.index') }}">
-            Frostnode CMS
+            <img src="{{ asset('/core/images/logo.svg') }}"> Frostnode CMS
         </a>
 
         <navbar-burger target="navMenu"></navbar-burger>
