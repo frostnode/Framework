e839f42b2802355ef232bc43c6007aff83187886
Kjell-Magnus Vike <magnus@vike.se>
2018-04-09 12:05:01 +0200

    Added correct settings migration
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

---
+++ modules/Settings/Database/migrations/2018_04_09_100242_create_settings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->increments('id');
+
+            // Data
+            $table->string('name');
+            $table->string('key')->unique();
+            $table->string('value');
+            $table->string('group'); // 1 = Draft, 2 = Published
+
+            // Indexes
+            $table->index([
+                'group'
+            ]);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
